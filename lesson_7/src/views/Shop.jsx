8104dd59877e7fbc4ec43207cfcc944bcead2999
Kintys <dima.sytnik.2017@gmail.com>
2025-12-18 16:35:16 +0100

    commit

---
+++ lesson_7/src/views/Shop.jsx
@@ -0,0 +1,14 @@
+import ProductsList from '../components/Product/ProductsList'
+import Container from '../layout/Container'
+
+function Shop() {
+    return (
+        <>
+            <Container>
+                <ProductsList />
+            </Container>
+        </>
+    )
+}
+
+export default Shop
